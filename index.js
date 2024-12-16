a456cf121f4836a5964cfde37a5e20317a9e318b
ShahAlom47 <sahalom4729@gmail.com>
2024-12-16 22:34:47 +0600

    Add buttons for selecting color and size

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const productImage= document.getElementById('productImage')
 const colorButtons = document.querySelectorAll('.color-button');
-const sizeButtons = document.querySelectorAll('.size-button'); // Added dot (.) to select size-button class
+const sizeButtons = document.querySelectorAll('.size-button');
+const btnDecrease = document.getElementById('quantity-decrease')
+const btnIncrease = document.getElementById('quantity-increase')
+const quantityDis = document.getElementById('quantity-dis')
+
+const imgPurple ='./assets/images/lg-a 3.jpg'
+const imgTeal ='./assets/images/cyan.jpg'
+const imgBlue ='./assets/images/lg-a 3 (1).jpg'
+const imgBlack ='./assets/images/black.jpg'
+
 let bandColor = 'button-purple';
-let bandSize = 'S';
+let productSize = 'S'
+let productPrice = 0
+let currentQuantity = 1;
+let totalPrice = 0;
 
 colorButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -12,6 +25,19 @@ colorButtons.forEach(button => {
 
         // Set the selected color
         bandColor = button.id;
+        if(button.id==='button-purple'){
+            productImage.src=imgPurple
+            
+        }
+        else if(button.id==='button-teal'){
+            productImage.src=imgTeal
+        }
+        else if(button.id==='button-blue'){
+            productImage.src=imgBlue
+        }
+        else if(button.id==='button-black'){
+            productImage.src=imgBlack
+        }
 
         // Add the border to the selected color button
         button.classList.add('border-2');
@@ -22,13 +48,30 @@ sizeButtons.forEach(button => {
     button.addEventListener('click', () => {
         // Remove the border from all size buttons
         sizeButtons.forEach(b => {
-            b.classList.remove('border-gray-50');
+            b.classList.remove('border-[#6576FF]');
         });
 
-        // Set the selected size
-        bandSize = button.id;
+        // Set the selected border
 
-        // Add the border to the selected size button
         button.classList.add('border-[#6576FF]');
+
+        productSize = button.id.split('-')[0]
+        productPrice = parseFloat(button.id.split('-')[1]).toFixed(2);
+
+        console.log(productPrice, productSize)
     });
 });
+
+
+btnIncrease.addEventListener('click', () => {
+    currentQuantity = currentQuantity + 1
+    quantityDis.innerText = currentQuantity
+})
+
+btnDecrease.addEventListener('click', () => {
+    if (currentQuantity > 1) {
+        currentQuantity = currentQuantity - 1
+        quantityDis.innerText = currentQuantity
+    }
+})
+
